0b2f8e9afd65280d216ba12bf96e559fb065c867
Gaius <gaiusdeveloper@gmail.com>
2025-07-04 23:43:04 +0000

    Create alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Authors(
+author_id INT AUTO_INCREMENT PRIMARY KEY,
+author_name VARCHAR(215)
+);
+
+CREATE TABLE Books(
+book_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(130),
+author_id INT,
+FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+price DOUBLE,
+publication_date DATE
+);
+
+CREATE TABLE Customers(
+customer_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+CREATE TABLE Orders(
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_id INT,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+order_date DATE
+);
+
+CREATE TABLE Order_Details(
+orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+order_id INT,
+book_id INT,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id),
+quantity DOUBLE
+);
